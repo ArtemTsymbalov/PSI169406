933bc6a26d5de6f76279f2863580405b4f7a1dde
ArtemTsymbalov <115550345+ArtemTsymbalov@users.noreply.github.com>
2024-10-15 15:17:22 +0200

    Update lab1_6.py

--- lab1_6.py
+++ lab1_6.py
@@ -1,32 +1,18 @@
 import asyncio
 import random
+import time
 
-
-async def fetch(delay: int) -> int:
+async def fetch(delay: int) -> float:
     await asyncio.sleep(delay)
     r =random.random()
     print(r)
     return r
 
-async def main() -> None:
-    await asyncio.gather(fetch(2))
-    await asyncio.gather(fetch(3))
-    await asyncio.gather(fetch(1))
-    print("end!")
-
+async  def main() -> None:
+    await asyncio.gather(fetch(2), fetch(1), fetch(3))
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)  # 1
-
-    task = loop.create_task(main())  # 2
-    loop.run_until_complete(task)  # 3
-
-    pending = asyncio.all_tasks(loop=loop)  # 4
-    for pending_task in pending:
-        pending_task.cancel()  # 5
-
-    group = asyncio.gather(*pending, return_exceptions=True)  # 6
-    loop.run_until_complete(group)  # 7
-
-    loop.close()  # 8
+    start=time.time()
+    with asyncio.Runner() as runner:
+        runner.run(main())
+    print(time.time() - start)
