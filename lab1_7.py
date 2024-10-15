355841e9cba131c7816ef7b072ba287ebb6422df
ArtemTsymbalov <115550345+ArtemTsymbalov@users.noreply.github.com>
2024-10-15 15:24:47 +0200

    Update lab1_7.py

--- lab1_7.py
+++ lab1_7.py
@@ -1,7 +1,22 @@
-async def foo() -> int:
-    return 10  # 1
+import asyncio
+import time
 
-
+async def krojenie() -> None:
+    await asyncio.sleep(2)
+    print("Gotowe krojenie")
+async def gotowanie() -> None:
+    await asyncio.sleep(5)
+    print("Gotowe gotowanie")
+async def smazenie() -> None:
+    await asyncio.sleep(3)
+    print("Gotowe smazenie")
 async def main() -> None:
-    result = await foo()  # 2
-    print(result)  # 3
+    await asyncio.gather(krojenie(), krojenie(), krojenie())
+    await asyncio.gather(krojenie(), gotowanie(), smazenie())
+    await asyncio.gather(smazenie(), smazenie(), smazenie())
+
+if __name__ == "__main__":
+    start=time.time()
+    with asyncio.Runner() as runner:
+        runner.run(main())
+    print(time.time() - start)
